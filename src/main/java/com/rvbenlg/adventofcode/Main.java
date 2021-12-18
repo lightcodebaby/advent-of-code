94c3922d3c857582e11a6967cd2acc6c2503cd23
Rubén <rubenlg94@gmail.com>
2021-12-18 15:41:53 +0100

    2021 - Day 17 - Part 1

--- src/main/java/com/rvbenlg/adventofcode/Main.java
+++ src/main/java/com/rvbenlg/adventofcode/Main.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Method;
 public class Main {
 
     public static final int YEAR = 2021;
-    public static final int DAY = 16;
+    public static final int DAY = 17;
 
     public static void main(String[] args) {
         if (args.length != 2) {
